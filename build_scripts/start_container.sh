ac42c9acde15bfb5f340bf0f5b4e89c7440f6675
Rob Broadley <git@rbroadley.co.uk>
2021-05-15 18:52:04 +0100

    Add ability to set working directory when starting container

--- build_scripts/start_container.sh
+++ build_scripts/start_container.sh
@@ -7,9 +7,16 @@ else
 	DIR="$(dirname "$0")"
 fi
 
+if [ -z $1 ]
+then
+	EXTRA_ARGS=""
+else
+	EXTRA_ARGS="--workdir=$(realpath $1)"
+fi
+
 (
 cd $DIR
 ./create_missing_host_files.sh
 cd ..
-docker-compose run --rm devenv
+docker-compose run --rm $EXTRA_ARGS devenv
 )
