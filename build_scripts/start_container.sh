6daae2477c3edc0db301961840a7f533f98fd772
Rob Broadley <git@rbroadley.co.uk>
2021-11-06 23:39:57 +0000

    Add down subcommand to devenv

--- build_scripts/start_container.sh
+++ build_scripts/start_container.sh
@@ -7,12 +7,20 @@ else
 	DIR="$(dirname "$0")"
 fi
 
-CONTAINER_ID=$(
-cd $DIR
-./create_missing_host_files.sh
-cd ..
-docker-compose up --detach
-echo $(docker-compose ps --quiet --filter image=devenv)
-)
+if [ "$1" == "down" ]
+then
+	(
+		cd $(dirname $DIR)
+		docker-compose down
+	)
+else
+	CONTAINER_ID=$(
+		cd $DIR
+		./create_missing_host_files.sh
+		cd ..
+		docker-compose up --detach
+		echo $(docker-compose ps --quiet --filter image=devenv)
+	)
 
-docker attach $CONTAINER_ID
+	docker attach $CONTAINER_ID
+fi
