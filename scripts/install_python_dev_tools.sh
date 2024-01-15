5b85785602ac59234c214d1fc634a1ae593c6d11
Rob Broadley <git@rbroadley.co.uk>
2024-01-15 18:46:57 +0000

    Fix error in version output of python dev tools install script

--- scripts/install_python_dev_tools.sh
+++ scripts/install_python_dev_tools.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Show the default Python 3 version.
-printf "Default Python is: %s\n" python3 --version
+printf "Default Python is: %s\n" "$(python3 --version)"
 
 # Generate list of installs including recent Python versions and tools for development.
 # Where Python versions other than the default are needed, it is expected that one of
